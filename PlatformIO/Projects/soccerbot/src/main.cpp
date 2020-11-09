a2c55eda0c549e957d2332adfe234afaa0e26c5d
Brad Larson <bhlarson@gmail.com>
2020-11-08 18:25:43 -0800

    simplify serial

--- PlatformIO/Projects/soccerbot/src/main.cpp
+++ PlatformIO/Projects/soccerbot/src/main.cpp
@@ -8,7 +8,8 @@ MeDCMotor motor1(M1);
 MeDCMotor motor2(M2);
 
 int16_t bri = 0, st = 0;
-uint8_t motorSpeed = 0;
+uint8_t device = 0;
+int8_t motorSpeed = 0;
 int loopcount = 0;
 const byte numChars = 32;
 char receivedChars[numChars]; // an array to store the received data
@@ -42,7 +43,26 @@ bool recvWithEndMarker() {
   }
   else {
     receivedChars[ndx] = '\0'; // terminate the string
-    motorSpeed = atoi(receivedChars);
+    int speed = atoi(receivedChars);
+    motor1.run(speed); // value: between -255 and 255.
+    motor2.run(speed); // value: between -255 and 255.
+    /*if(ndx > 6)
+    {
+      int8_t speed;
+      receivedChars[2] = '\0';
+      device = atoi(receivedChars);
+      switch(device){
+        case 1:
+          speed = atoi(&(receivedChars[3]));
+          motor1.run(speed); // value: between -255 and 255.
+          break;
+        case 2:
+          speed = atoi(&(receivedChars[3]));
+          motor2.run(speed); // value: between -255 and 255.
+          break;
+      }
+    }
+    */
     ndx = 0;
     newData = true;
   }
@@ -50,36 +70,29 @@ bool recvWithEndMarker() {
  return newData;
 }
 
-void loop() {
+void loop() 
+{
   loopcount++;
   Serial.println(loopcount);
 
-  if(recvWithEndMarker()){
+  if(recvWithEndMarker())
+  {
 
   }
 
-  if (loopcount % 1000){
-    passes =  loopcount/1000
+  if (loopcount % 500){
+    int passes = loopcount/500;
     if(passes%2)
     {
-      led1.setColor(1, 255, 0, 0); // parameter description: led number, red, green, blue
-      led1.setColor(2, 0, 255, 0); // parameter description: led number, red, green, blue
+      led1.setColor(1, 255, 0, 255); // parameter description: led number, red, green, blue
+      led1.setColor(2, 255, 255, 0); // parameter description: led number, red, green, blue
       led1.show();
     }
     else
     {
-      led1.setColor(1, 0, 255, 0); // parameter description: led number, red, green, blue
-      led1.setColor(2, 255, 0, 0); // parameter description: led number, red, green, blue
+      led1.setColor(1, 0, 255, 255); // parameter description: led number, red, green, blue
+      led1.setColor(2, 255, 0, 255); // parameter description: led number, red, green, blue
       led1.show();
     }
-
-  motor1.run(motorSpeed); // value: between -255 and 255.
-  motor2.run(motorSpeed); // value: between -255 and 255.
-  // turn the LED on (HIGH is the voltage level)
-  //digitalWrite(LED_BUILTIN, HIGH);
-
-  // turn the LED off by making the voltage LOW
-  //digitalWrite(LED_BUILTIN, LOW);
-
-
+  }
 }
